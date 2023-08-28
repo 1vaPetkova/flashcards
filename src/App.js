4a0776226434b3a19eaa06bdc738d16a97575d26
Iva Petkova <iva1petkova@gmail.com>
2023-08-28 22:46:21 +0300

    add flashcards

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { questions } from "./Data.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flashcards">
+      {questions.map((q) => (
+        <FlashCard question={q} />
+      ))}
+    </div>
+  );
+}
+
+function FlashCard({ question }) {
+  const [selected, setSelected] = useState(false);
+
+  return (
+    <div
+      className={selected ? "selected" : ""}
+      onClick={() => setSelected(!selected)}
+    >
+      {selected ? question.answer : question.question}
     </div>
   );
 }
